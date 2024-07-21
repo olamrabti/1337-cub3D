1f87ae9d4274737faf5a5ae747c92d80dc4c0625
olamrabti <oussamalamrabtii@gmail.com>
2024-07-21 20:34:53 +0100

    all setup, parsing works good as a beginning

--- cub3d.h
+++ cub3d.h
@@ -1,9 +1,10 @@
 #ifndef CUB3D_H
 #define CUB3D_H
 
-#include "./MLX42/include/MLX42/MLX42.h"
-#include "./Libft/libft.h"
-#include "./get_next_line/get_next_line.h"
+// #include "./MLX42/include/MLX42/MLX42.h"
+#include "MLX42/include/MLX42/MLX42.h"
+#include "Libft/libft.h"
+#include "get_next_line/get_next_line.h"
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
