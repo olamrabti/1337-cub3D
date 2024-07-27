a573e49c21e557eb5a9bd3f1af3f1f5aad743974
olamrabti <oussamalamrabtii@gmail.com>
2024-07-27 22:14:51 +0100

    implementing dda algo for raycasting : something wrong with first x it gives big negative values

--- src/drawing_utils.c
+++ src/drawing_utils.c
@@ -51,8 +51,8 @@ void draw_circle(mlx_image_t *img, int x_center, int y_center)
 
 double get_distance(t_data data, int x_wall, int y_wall, int num_pixels)
 {
-    if (num_pixels < 2)
-        return 0.0;
+    // if (num_pixels < 1)
+        // return 0.0;
     double total_distance = 0.0;
     int dx = x_wall - data.player.x;
     int dy = y_wall - data.player.y;
@@ -98,7 +98,7 @@ int is_wall(t_data data, int x, int y)
 
     // printf("map : %c", data.map[y / SIZE][x / SIZE]);
     // printf("x /size : %d, y / size : %d\n", x /SIZE, y/SIZE );
-    if (y < 0 || x < 0 || y > (data.lines * SIZE) - SIZE || x  > (ft_strlen(data.map[y / SIZE]) * SIZE - SIZE))
+    if (y < 0 || x < 0 || y - (SIZE / 2) > (data.lines * SIZE) || x - (SIZE / 2) > (ft_strlen(data.map[y / SIZE]) * SIZE))
         return 0;
     if (data.map[y / SIZE][x / SIZE] == '1')
         return 1;
@@ -115,41 +115,107 @@ void clear_screen(mlx_image_t *img, int color)
     }
 }
 
-void render_wall(t_data data, int distance , int x)
+// void render_wall(t_data data, int distance , int x)
+// {
+//     int wall_height;
+//     int wall_top;
+//     int wall_bottom;
+//     double scale;
+
+//     scale = 2;
+//     if (!distance)
+//         distance = 1;
+//     wall_height = ((int)(HEIGHT / distance) * scale);
+//     wall_top = (HEIGHT - wall_height) / 2;
+//     wall_bottom = (HEIGHT + wall_height) / 2;
+
+//     // printf("wall_top : %d\n", wall_top);
+//     while (wall_top < wall_bottom)
+//     {
+//         wall_top++;
+//         protected_ppx(data.img, x , wall_top, get_rgba(255, 200, 0, 255));
+//     }
+// }
+
+// void draw_rays(t_data data)
+// {
+//     double angle;
+//     double corrected_distance;
+//     int distance;
+//     int x;
+//     double ray_angle;
+    
+//     angle = data.player.rotation_angle - (M_PI / 6);
+//     while (angle <= data.player.rotation_angle + (M_PI / 6))
+//     {
+//         ray_angle = angle - data.player.rotation_angle;
+//         distance = draw_line(data, data.player.x, data.player.y, data.player.x + cos(angle) * WIDTH, data.player.y + sin(angle) * HEIGHT);
+//         corrected_distance = distance * cos(ray_angle);
+//         x = (angle - (data.player.rotation_angle - (M_PI / 6))) / (M_PI / 3) * WIDTH;
+//         render_wall(data, corrected_distance, x);
+//         angle += (M_PI / 3) / WIDTH;
+//     }
+// }
+#include <math.h>  // For M_PI and fmod
+
+double normalize_angle(double angle) 
 {
-    int wall_height;
-    int wall_top;
-    int wall_bottom;
-    double scale;
-
-    scale = 2;
-    if (!distance)
-        distance = 1;
-    wall_height = ((int)(HEIGHT / distance) * scale);
-    wall_top = (HEIGHT - wall_height) / 2;
-    wall_bottom = (HEIGHT + wall_height) / 2;
-
-    // printf("wall_top : %d\n", wall_top);
-    while (wall_top < wall_bottom)
-    {
-        wall_top++;
-        protected_ppx(data.img, x , wall_top, get_rgba(255, 200, 0, 255));
+    double normalized = fmod(angle, 2 * M_PI);
+    if (normalized < 0) {
+        normalized += 2 * M_PI;
     }
+    return normalized;
 }
 
+// int draw_line();
+int floor_division(int a, int b)
+{
+    double result = (double)a / b;
+    return (int)floor(result);
+}
+
+int is_up(double angle) 
+{
+    return (angle >= M_PI / 2 && angle < M_PI);
+}
+
+int is_right(double angle) 
+{
+    return (angle >= 0 && angle < M_PI / 2); 
+}
 void draw_rays(t_data data)
 {
     double angle;
     int distance;
-    int x;
+    double half_fov;
+    double ray_angle;
+    int i;
+    int delta_x;
+    int delta_y;
+    int first_x;
+    int first_y;
 
-    angle = data.player.rotation_angle - (M_PI / 6);
-    while (angle <= data.player.rotation_angle + (M_PI / 6))
+    half_fov = (M_PI / 6);
+    ray_angle = 2 * half_fov / WIDTH;
+    angle = normalize_angle(data.player.rotation_angle - half_fov);
+    i = 0;
+    while (i++ < 1)
     {
-        distance = draw_line(data, data.player.x, data.player.y, data.player.x + cos(angle) * WIDTH, data.player.y + sin(angle) * HEIGHT);
-        x = (angle - (data.player.rotation_angle - (M_PI / 6))) / (M_PI / 3) * WIDTH;
-        // printf("x : %d\n", x);
-        render_wall(data, distance, x);
-        angle += (M_PI / 6) / (WIDTH);
+        delta_y = SIZE;
+        delta_x = SIZE / tan (angle);
+        first_y = floor_division(data.player.y , SIZE) * SIZE;
+        first_x = data.player.x + ((first_y - data.player.y)/ tan(angle));
+        if (!is_up(angle))
+            first_y += SIZE;
+        else
+            delta_y *= -1;
+        if (!is_right(angle) && delta_x > 0)
+            delta_x *= -1;
+        if (is_right(angle) && delta_x < 0)
+            delta_x *= -1;
+        printf("first_x : %d, first_y : %d \n", first_x, first_y);
+        printf(" x : %d, y : %d \n", data.player.x, data.player.y);
+        // distance = draw_line(data, data.player.x, data.player.y, data.player.x + first_x, data.player.y + first_y);
+        angle = normalize_angle(angle + ray_angle);
     }
 }
